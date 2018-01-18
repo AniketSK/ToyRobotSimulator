d79fe9690768b47051ff2cc33ba2e733c6ca8caf
Aniket Kadam <anikadamg@gmail.com>
2018-01-18 10:57:25 +0530

    Added a grid that will define the area that is valid for the bot to move

---
+++ app/src/main/java/com/aniketkadam/toyrobotsimulator/movementgrid/MovementGrid.kt
@@ -0,0 +1,6 @@
+package com.aniketkadam.toyrobotsimulator.movementgrid
+
+/**
+ * Defines the grid that will be used to determine valid positions for the bot.
+ */
+data class MovementGrid(val gWidth : Int, val gHeight : Int)
