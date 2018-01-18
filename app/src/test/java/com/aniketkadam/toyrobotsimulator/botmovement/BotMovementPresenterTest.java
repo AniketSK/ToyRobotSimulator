65ac8afd2dad52e2e7a96ca7154dc33957cc0ff4
Aniket Kadam <anikadamg@gmail.com>
2018-01-18 11:00:29 +0530

    Empty test for bot movement.
    This will be used to define conditions that are required for proper bot
    movement.

---
+++ app/src/test/java/com/aniketkadam/toyrobotsimulator/botmovement/BotMovementPresenterTest.java
@@ -0,0 +1,48 @@
+package com.aniketkadam.toyrobotsimulator.botmovement;
+
+import com.aniketkadam.toyrobotsimulator.botposition.BotPositionModel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Behavioural test for bot movement.
+ */
+public class BotMovementPresenterTest {
+
+    BotPositionModel model;
+    BotMovementPresenter botMovementPresenter;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void place() throws Exception {
+    }
+
+    @Test
+    public void moveForward() throws Exception {
+    }
+
+    @Test
+    public void moveLeft() throws Exception {
+    }
+
+    @Test
+    public void moveRight() throws Exception {
+    }
+
+    @Test
+    public void reportPosition() throws Exception {
+    }
+
+}
