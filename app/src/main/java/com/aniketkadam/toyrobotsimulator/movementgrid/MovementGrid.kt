605c9d0602b6aa35229adfef06cd8af447282755
Aniket Kadam <anikadamg@gmail.com>
2018-01-18 11:24:19 +0530

    Movement grid was modified to specify a lower bound.
    This is because there was no low bound previously specified and would've
    had to be implicit. The fact that this exists was made explicit.

--- app/src/main/java/com/aniketkadam/toyrobotsimulator/movementgrid/MovementGrid.kt
+++ app/src/main/java/com/aniketkadam/toyrobotsimulator/movementgrid/MovementGrid.kt
@@ -2,5 +2,9 @@ package com.aniketkadam.toyrobotsimulator.movementgrid
 
 /**
  * Defines the grid that will be used to determine valid positions for the bot.
+ * Assumes a grid with lower and upper x (width) bounds and lower and upper y (height) bounds
  */
-data class MovementGrid(val gWidth : Int, val gHeight : Int)
+data class MovementGrid(val lbX : Int,
+                        val lbY: Int,
+                        val ubX : Int,
+                        val ubY : Int)
