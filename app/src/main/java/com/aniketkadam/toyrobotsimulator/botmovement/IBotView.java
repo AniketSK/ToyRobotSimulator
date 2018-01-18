6ae19c16d81e0cf1b9d2ba3fd2739b4a5429318d
Aniket Kadam <anikadamg@gmail.com>
2018-01-18 14:38:16 +0530

    Added a view interface for the activity functions

---
+++ app/src/main/java/com/aniketkadam/toyrobotsimulator/botmovement/IBotView.java
@@ -0,0 +1,11 @@
+package com.aniketkadam.toyrobotsimulator.botmovement;
+
+import com.aniketkadam.toyrobotsimulator.base.BaseView;
+
+/**
+ * Defines the view interface for the bot view.
+ */
+
+public interface IBotView extends BaseView {
+    void showReport(String report);
+}
