bbdb90b6dbf137cff052f67cbd467f2e967c87ec
Aniket Kadam <anikadamg@gmail.com>
2018-01-18 11:07:16 +0530

    Removed erroneous import.
    The file it refers to was removed and replaced.

--- app/src/main/java/com/aniketkadam/toyrobotsimulator/botposition/BotPositionModel.kt
+++ app/src/main/java/com/aniketkadam/toyrobotsimulator/botposition/BotPositionModel.kt
@@ -1,7 +1,5 @@
 package com.aniketkadam.toyrobotsimulator.botposition
 
-import com.aniketkadam.toyrobotsimulator.botposition.BotDirection.Direction
-
 /**
  * Represents the position and orientation of the robot on the board.
  */
